7fb40358ee7410940e091216580c0aefcfec5ed3
vibecastor <vibecastor@gmail.com>
2018-02-13 14:54:34 -0800

    Completed third object

--- js/app.js
+++ js/app.js
@@ -92,3 +92,48 @@ store2.customersPerHour();
 store2.cookiesPerHour();
 store2.calcTotalCookiesSoldPerDay();
 store2.addElement();
+
+var store3 = {
+  minCustomerPerHour: 11,
+  maxCustomerPerHour: 38,
+  avgCookiesPerSale: 3.7,
+  customersPerHourArray: [],
+  cookiesPerHourArray: [],
+  customersPerHour: function () {
+    for (var i = 0; i < hoursPerDay.length; i++) {
+      this.customersPerHourArray.push(Math.floor(Math.random() * (this.maxCustomerPerHour - this.minCustomerPerHour + 1)) + this.minCustomerPerHour);
+    }
+  },
+  cookiesPerHour: function() {
+    for (var i = 0; i < hoursPerDay.length; i++) {
+      this.cookiesPerHourArray.push(Math.floor(this.customersPerHourArray[i] * this.avgCookiesPerSale));
+    }
+  },
+  calcTotalCookiesSoldPerDay: function() {
+    this.totalCookiesSoldPerDay = 0;
+    for (var i = 0; i < hoursPerDay.length; i++) {
+      this.totalCookiesSoldPerDay += this.cookiesPerHourArray[i];
+    }
+  },
+  addElement: function () {
+    var ulEl = document.getElementById('Seattle Center');
+    for (var i = 0; i < this.cookiesPerHourArray.length; i++) {
+      //create an element
+      var liEl = document.createElement('li');
+      //adding content
+      liEl.textContent = hoursPerDay[i] + ': ' + this.cookiesPerHourArray[i] + ' cookies';
+      //appending to the DOM
+      //parent.appendChild(child)
+      ulEl.appendChild(liEl);
+    }
+    liEl = document.createElement('li');
+    //"Total:  657 cookies"
+    liEl.className = 'total';
+    liEl.textContent = 'Total ' + this.totalCookiesSoldPerDay + ' cookies';
+    ulEl.appendChild(liEl);
+  },
+};
+store3.customersPerHour();
+store3.cookiesPerHour();
+store3.calcTotalCookiesSoldPerDay();
+store3.addElement();
