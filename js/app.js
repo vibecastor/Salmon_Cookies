1ce776239d8ab652fd86ef4c67e3be2092c26dcb
vibecastor <michaelcastor@Michaels-MacBook-Pro.local>
2018-02-13 13:45:19 -0800

    Adding total cookies per day function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-var hoursPerDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+'use strict';
+
+var hoursPerDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 var store1 = {
   minCustomerPerHour: 23,
@@ -16,13 +18,20 @@ var store1 = {
       this.cookiesPerHourArray.push(Math.floor(this.customersPerHourArray[i] * this.avgCookiesPerSale));
     }
   },
+  calcTotalCookiesSoldPerDay: function() {
+    this.totalCookiesSoldPerDay = 0;
+    for (var i = 0; i < hoursPerDay.length; i++) {
+      this.totalCookiesSoldPerDay += this.cookiesPerHourArray[i];
+    }
+  },
   addElement: function () {
     var ulEl = document.getElementById('1st and Pike');
     for (var i = 0; i < this.cookiesPerHourArray.length; i++) {
       //create an element
       var liEl = document.createElement('li');
       //adding content
-      liEl.textContent = hoursPerDay[i] + ': ' + this.cookiesPerHourArray[i];
+      liEl.textContent = hoursPerDay[i] + ': ' + this.cookiesPerHourArray[i] + ' cookies';
+      //concatinate 'cookies' in line 29
       //appending to the DOM
       //parent.appendChild(child)
       ulEl.appendChild(liEl);
@@ -31,8 +40,8 @@ var store1 = {
 };
 store1.customersPerHour();
 store1.cookiesPerHour();
+store1.calcTotalCookiesSoldPerDay();
 store1.addElement();
-
 
 
 
@@ -45,4 +54,3 @@ store1.addElement();
 //   customersPerHour [ ] need a method to calculate it...
 //   totalCookiesNeededPerDay:   need a method to calc. [i]
 // 	cookiesSoldPerHour = [ ] need a method to calculate it...
-// render  need a method
